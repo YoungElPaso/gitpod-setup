65ab0347f20e6c9be4d159d53c7f01cd48d3dc12
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2022-01-25 16:14:48 +0000

    Messed up the order of tasks - need to use init and command,  not before

--- .gitpod-scripts/fish-install.sh
+++ .gitpod-scripts/fish-install.sh
@@ -2,8 +2,8 @@
 
 # Symlink Fish shared directory (history etc) to ~/workspace so it can persist across workspace reload. (/workspace is persisted by Gitpod!)
 # Create workspace .fish dir, history persists here.
-mkdir -p /workspace/.fish/
+# mkdir -p /workspace/.fish/
 # Create share dir, fish config has to go here - fish only allows symlink to this dir.
-mkdir -p ~/.local/share/
+# mkdir -p ~/.local/share/
 # Symlink share to persisted workspace fish dir.
 ln -s /workspace/.fish ~/.local/share/fish
